4aaea6dd9933c84cbc9472873323c3ec3764ea5f
MauroLucero <mauro.luceror@gmail.com>
2023-03-31 18:35:49 -0300

    Modified altaPaciente HTML, CSS and JS

---
+++ src/main/resources/static/js/postPaciente.js
@@ -0,0 +1,67 @@
+window.addEventListener("load", function () {
+  const formulario = this.document.querySelector("#add_new_paciente");
+
+  fetch("http://localhost:8080/pacientes/")
+    .then((response) => response.json())
+    .then((data) => {
+      const pacientesDiv = document.getElementById("pacientes");
+      const listaPacientes = document.createElement("ul");
+      listaPacientes.setAttribute("class","paciente-list")
+
+      data.forEach((paciente) => {
+        listaPacientes.innerHTML +=
+          '<li class="paciente-item"><div class="paciente-info"><h3 class="paciente-nombre">' +
+          paciente.nombre +
+          " " +
+          paciente.apellido +
+          '</h3><p class="paciente-dni">DNI: ' +
+          paciente.dni +
+          '</p></div><div class="paciente-actions"><button class="editar-button">Editar</button><button class="borrar-button">Borrar</button></div></li>';
+      });
+
+      pacientesDiv.appendChild(listaPacientes);
+    });
+
+  formulario.addEventListener("submit", function (event) {
+    const formDataPaciente = {
+      nombre: document.querySelector("#nombre").value,
+      apellido: document.querySelector("#apellido").value,
+      email: document.querySelector("#email").value,
+      dni: document.querySelector("#dni").value,
+      fechaIngreso: document.querySelector("#fecha").value,
+      domicilio: {
+        calle: document.querySelector("#calle").value,
+        numero: document.querySelector("#numeroCalle").value,
+        localidad: document.querySelector("#localidad").value,
+        provincia: document.querySelector("#provincia").value,
+      },
+    };
+
+    console.log(formDataPaciente);
+
+    fetch("http://localhost:8080/pacientes/", {
+      method: "POST", // or 'PUT'
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(formDataPaciente),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("Success:", data);
+      })
+
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  });
+});
+
+const botonMostrar = document.getElementById("mostrar-formulario");
+const formulario = document.getElementById("add_new_paciente");
+
+botonMostrar.addEventListener("click", function () {
+  if (formulario.style.display === "none") {
+    formulario.style.display = "block";
+  } else {
+    formulario.style.display = "none";
+  }
+});
